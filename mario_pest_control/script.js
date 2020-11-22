65542c806d58b9b8bf62b99bae40e2fdc619f791
claudiacole96 <claudia.evans@bryanuniversity.edu>
2020-11-22 10:35:54 -0500

    week6

---
+++ mario_pest_control/script.js
@@ -0,0 +1,18 @@
+/*
+Princess Peach has a huge infestation of Goombas, Bob-ombs, and Cheep-cheeps, and 
+has commissioned Mario to take care of her pest problem. Mario did the job, but 
+he is having trouble keep track of how many he has caught and has asked you to 
+build him a website that keeps track of how many baddies he caught for Princess 
+Peach so he can send her a bill.
+
+Pricing
+    Goombas: 5 Coins
+    Bob-ombs: 7 Coins
+    Cheep-cheeps: 11 Coins
+    Baddies Caught
+
+The number of baddies caught will be entered into their input fields. Each baddies type will have their own input field.
+
+Total Price
+    This will be dynamic, depending on how many baddies were caught.
+*/
