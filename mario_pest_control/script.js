a2c291b8541939adecd9df94167794d731a3b5b3
jesse hernandez <mrjesse1283>
2020-11-28 15:35:31 -0700

    mario_pest_control

--- mario_pest_control/script.js
+++ mario_pest_control/script.js
@@ -1,18 +1,15 @@
-/*
-Princess Peach has a huge infestation of Goombas, Bob-ombs, and Cheep-cheeps, and 
-has commissioned Mario to take care of her pest problem. Mario did the job, but 
-he is having trouble keep track of how many he has caught and has asked you to 
-build him a website that keeps track of how many baddies he caught for Princess 
-Peach so he can send her a bill.
+/* jshint esversion:6 */
 
-Pricing
-    Goombas: 5 Coins
-    Bob-ombs: 7 Coins
-    Cheep-cheeps: 11 Coins
-    Baddies Caught
+const totalButton = document.getElementById("myTotal");
+totalButton.addEventListener("click", totalPrice);
 
-The number of baddies caught will be entered into their input fields. Each baddies type will have their own input field.
+function totalPrice() {
+    const goomba = parseInt(document.getElementById("goomba").value);
+    const bobOmb = parseInt(document.getElementById("bobomb").value);
+    const cheepEep = parseInt(document.getElementById("cheep").value);
+    
+    let result = document.getElementById("inputTotal");
+    let total = (goomba * 5) + (bobOmb * 7) + (cheepEep * 11);
 
-Total Price
-    This will be dynamic, depending on how many baddies were caught.
-*/
+    result.innerHTML = `${total} COINS`;
+}
